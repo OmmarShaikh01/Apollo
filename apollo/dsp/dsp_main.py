873c207b32caf8cb636fefdfd511045022c81481
UglyClown999 <ommarshaikh17@gmail.com>
2021-02-07 23:55:02 +0530

    Chages to DSP, added a mixer,

--- apollo/dsp/dsp_main.py
+++ apollo/dsp/dsp_main.py
@@ -3,7 +3,8 @@ from PyQt5 import QtWidgets, QtCore, QtGui
 
 import sys, os, time, math
 
-from apollo.dsp.plugins.SignalProcessors import MasterProcessor, VUMeter
+from apollo.dsp.plugins.SignalProcessors import MasterProcessor
+from apollo.dsp.plugins.apollo_mixer import ApolloAudioMixer
 
 class ComputeGraph:
     """"""
@@ -43,7 +44,10 @@ class ApolloDSP:
             from apollo.app.apollo_TabBindings import ApolloTabBindings
             self.UI = ApolloTabBindings()
 
+        self.Mixer = ApolloAudioMixer(self.UI.apollo_WDG_ATOL_mixer)
+        self.Mixer.create_EmptyMixerChannels(35)
         self.StartServer()
+
         self.MasterProcessorBindings(pyo.Noise())
         self.ButtonBindings()
 
@@ -53,8 +57,7 @@ class ApolloDSP:
         """
         self.MasterProcessor = MasterProcessor(Input)
         self.MasterProcessor.processor().out()
-        self.MasterProcessor.SetMixer_Channel(self.UI.apollo_WDG_ATOL_mixer)
-
+        self.Mixer.bind_ProcessortoMixer(self.MasterProcessor, 0)
 
     def ButtonBindings(self):
         self.UI.apollo_DIAL_ATOL_masterVU_chnl.valueChanged.connect(lambda x: self.Server.setAmp(x /1000))
@@ -82,29 +85,10 @@ class ApolloDSP:
         if not self.Server.start():
             raise ConnectionError("Audio Server Failed To start")
 
-        # sets tehe VU meter for the server Amp
-        MasterVU_Meter = VUMeter(Meter = self.UI.apollo_PIXLB_ATOL_masterVU_VU)
-
-        # adds an scaling factor for displaying the amplitude
-        MasterVU_Meter.scale = 100
-
-        # binds the server callback to the meter painter
-        self.Server.setMeterCallable(MasterVU_Meter.Painter)
-
         self.Server.setAmp(kwargs.get("amp", 0.000))
+        self.Mixer.connect_ServerChannel(self.Server, self.UI.apollo_PIXLB_ATOL_masterVU_VU)
         self.UI.apollo_DIAL_ATOL_masterVU_chnl.setValue(kwargs.get("amp", 0.001))
 
-        try:
-            self.MasterVU_Meter1 = VUMeter(Parent = self.UI.apollo_WDG_ATOL_mixer)
-            self.MasterVU_Meter2 = VUMeter(Parent = self.UI.apollo_WDG_ATOL_mixer)
-            self.MasterVU_Meter3 = VUMeter(Parent = self.UI.apollo_WDG_ATOL_mixer)
-            self.MasterVU_Meter4 = VUMeter(Parent = self.UI.apollo_WDG_ATOL_mixer)
-            self.MasterVU_Meter5 = VUMeter(Parent = self.UI.apollo_WDG_ATOL_mixer)
-            self.MasterVU_Meter6 = VUMeter(Parent = self.UI.apollo_WDG_ATOL_mixer)
-            self.MasterVU_Meter7 = VUMeter(Parent = self.UI.apollo_WDG_ATOL_mixer)
-            self.MasterVU_Meter8 = VUMeter(Parent = self.UI.apollo_WDG_ATOL_mixer)
-        except Exception as e:
-            print(e)
 
     def StopServer(self):
         """
